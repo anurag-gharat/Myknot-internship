564987fbdcf82bfcc7b3454c7f016d97e5d7c845
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-01 18:34:21 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.collapsible').collapsible();
+  });
